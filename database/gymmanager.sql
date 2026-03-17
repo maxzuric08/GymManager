6de02a659e592d9b025939e361ccb4f9c5dfb361
maxzuric08 <mjzunigaricardi@mail.austral.edu.ar>
2026-03-17 11:09:03 -0300

    Backup db

---
+++ database/gymmanager.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict j8uheoSvOVoEGTOFg4pm8yfcO2dTfcbYo6eprx9xWiTyeqDm8R4NzeBfW5ff5UG
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+-- Started on 2026-03-17 11:08:12
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-03-17 11:08:13
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict j8uheoSvOVoEGTOFg4pm8yfcO2dTfcbYo6eprx9xWiTyeqDm8R4NzeBfW5ff5UG
+
