f18e49ffcf2ddb8864d5604b7f9a778b0efc1f8c
renki <renkineluciano1@gmail.com>
2026-03-17 11:23:28 -0300

    created first tables

--- database/gymmanager.sql
+++ database/gymmanager.sql
@@ -1,31 +1,41 @@
---
--- PostgreSQL database dump
---
+-- Planes
+CREATE TABLE IF NOT EXISTS plans (
+    plan_id SERIAL PRIMARY KEY,
+    plan_type VARCHAR(50) NOT NULL,
+    cost DECIMAL(10, 2) NOT NULL,
+    duration VARCHAR(50),
+    benefits TEXT,
+    class_limit INTEGER,
+    status VARCHAR(20) DEFAULT 'active'
+);
 
-\restrict j8uheoSvOVoEGTOFg4pm8yfcO2dTfcbYo6eprx9xWiTyeqDm8R4NzeBfW5ff5UG
+-- Sedes
+CREATE TABLE IF NOT EXISTS branches (
+    branch_id SERIAL PRIMARY KEY,
+    location VARCHAR(100) NOT NULL,
+    size VARCHAR(50)
+);
 
--- Dumped from database version 18.3
--- Dumped by pg_dump version 18.3
+-- Plan-Sede
+CREATE TABLE IF NOT EXISTS plan_branch (
+    plan_id INTEGER REFERENCES plans(plan_id) ON DELETE CASCADE,
+    branch_id INTEGER REFERENCES branches(branch_id) ON DELETE CASCADE,
+    PRIMARY KEY (plan_id, branch_id)
+);
 
--- Started on 2026-03-17 11:08:12
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
--- Completed on 2026-03-17 11:08:13
-
---
--- PostgreSQL database dump complete
---
-
-\unrestrict j8uheoSvOVoEGTOFg4pm8yfcO2dTfcbYo6eprx9xWiTyeqDm8R4NzeBfW5ff5UG
-
+-- Usuarios
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    branch_id INTEGER REFERENCES branches(branch_id), -- Home branch
+    plan_id INTEGER REFERENCES plans(plan_id),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    dni VARCHAR(20) UNIQUE NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    user_status VARCHAR(20) DEFAULT 'active',
+    birth_date DATE,
+    registration_date DATE DEFAULT CURRENT_DATE
+);
